64c36257f65b03d6e207df0eb0fa530c2a627455
javckzack <javck.zack@gmail.com>
2022-12-30 10:04:30 +0800

    網頁元素導用示範

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Element;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
     public function index(){
-        return view('index');
+        $sliders = Element::where('page','index')->where('position','slider')->orderBy('sort','asc')->get();
+        return view('index',compact('sliders'));
     }
 
     public function shop(){
